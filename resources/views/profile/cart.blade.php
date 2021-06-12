fd53de0c977cfdc9c02aae63f06f6392647ee425
ktheofilus <ktheofilus@gmail.com>
2021-06-12 23:43:29 +0700

    fix wrong item list

--- resources/views/profile/cart.blade.php
+++ resources/views/profile/cart.blade.php
@@ -29,12 +29,10 @@
         @endif
 
         <?php $total=0 ?>
-
         @if(empty($items))
 
         @foreach ($items as $item)
-
-        <?php $total += $item->price; ?>
+        <?php $total += $item->price ?>
             <div class="card my-2" style="height: 100px;">
                 <div class="row my-2" style="height: auto">
                     <div class="row  clickable-row" data-href='/item/{{$item->id}}'>
--- resources/views/profile/itemlist.blade.php
+++ resources/views/profile/itemlist.blade.php
@@ -2,7 +2,6 @@
 
 
 @section('profilecontainer')
-
     <style>
         .clickable-row{
     cursor:pointer;
@@ -19,20 +18,13 @@
 
     <div class="row p-3">
         <p class="h1 fw-bolder border border-3 border-start-0 border-end-0">
-            Cart
+            Item List
         </p>
 
-        @if ($errors->all())
-            <p class="h5 text-danger fw-bolder border border-3 border-start-0 border-end-0">
-                {{$errors->first()}}
-            </p>
-        @endif
-
-        <?php $total=0 ?>
-        @if(empty($items))
-
         @foreach ($items as $item)
-        <?php $total += $item->price ?>
+
+
+
             <div class="card my-2" style="height: 100px;">
                 <div class="row my-2" style="height: auto">
                     <div class="row  clickable-row" data-href='/item/{{$item->id}}'>
@@ -40,8 +32,8 @@
                         <div class="col-1">
                             <img src="/images/{{$item->photo}}" class="card-img-top img-responsive" alt="...">
                         </div>
+
                         <div class="col">
-                            
                             <div class="row">
                                 <h5 class="title">{{$item->itemname}}</h5>
                             </div>   
@@ -50,38 +42,20 @@
                             </div>
                         </div>
                         <div class="col-1">
-                            <a href="/deletecart/{{$item->id}}" class="btn btn-danger flex">Delete</a>
+                            <form action="/deleteitem/{{$item->id}}" method="post">
+                                @csrf
+                                <button type="submit" class="btn btn-danger flex">Delete</button>
+                            </form>
                         </div>
 
                     </div>
-                        
+
                 </div>                    
             </div>
 
-            
+
         @endforeach
 
-        @endif
-
-        @if($total>0)
-
-        <div class="card-footer text-end h5 ">Total : Rp.<?= $total; ?></div>
-
-        <form method="POST" action="/checkout" id="checkout">@csrf</form>
-
-        <button form="checkout" class="btn btn-success flex">Check Out!</button>
-            
-        @else
-
-        <p class="h3 fw-bolder border border-3 border-start-0 border-end-0">
-            Keranjang Kosong! <br>
-            Masukan beberapa barang ke keranjangmu!
-        </p>
-            
-        @endif
 
 
-        
-    </div>
-    
 @endsection
